f85e151d1990ae3fce83d21d00a6610eb6926dba
Thomas <thomassharp2003@gmail.com>
2024-12-05 22:15:40 +0100

    Impulso al trigger

--- src/distance.c
+++ src/distance.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pigpio.h>
+#include "distance.h"
 
 void main(){
   if (gpioInitialise() < 0)
@@ -9,9 +10,12 @@ void main(){
     exit(1);
   }
   // From the GPIO 16 pin we are going to read (ECHO 3V)
-  gpioSetMode(16, PI_INPUT);
+  gpioSetMode(ECHO, PI_INPUT);
   // From the GPIO 5 pin we are going to send a TTL pulse (TRIG)
-  gpioSetMode(5, PI_OUTPUT);
+  gpioSetMode(TRIG, PI_OUTPUT);
+
+  // Sends a 10us pulse to the TRIG
+  gpioTrigger(TRIG, 10, 1);
 
 
 
---
+++ src/distance.h
@@ -0,0 +1,7 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+
+#define TRIG 25
+#define ECHO 27
+
+#endif
