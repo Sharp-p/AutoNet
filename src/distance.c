c567ecd4b37f98b992be8fb36b5c5233ef76aa45
Thomas Sharp <thomassharp2003@gmail.com>
2024-12-05 20:30:07 +0100

    Set pin IO

--- src/distance.c
+++ src/distance.c
@@ -1,4 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <pigpio.h>
 
-void main(){}
+void main(){
+  if (gpioInitialise() < 0)
+  {
+    perror("[ERROR]Couldn't initialize GPIO interface.");
+    exit(1);
+  }
+  // From the GPIO 16 pin we are going to read (ECHO 3V)
+  gpioSetMode(16, PI_INPUT);
+  // From the GPIO 5 pin we are going to send a TTL pulse (TRIG)
+  gpioSetMode(5, PI_OUTPUT);
+
+
+
+  gpioTerminate();
+  
+}
