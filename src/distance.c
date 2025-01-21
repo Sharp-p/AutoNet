0f0cbc22775f70855a69665cc83a88601f9e57ec
Thomas Sharp <thomassharp2003@gmail.com>
2025-01-21 10:46:15 +0100

    Codice sensore distanza funzionante

--- src/distance.c
+++ src/distance.c
@@ -6,8 +6,9 @@
 #include "distance.h"
 
 int main(){
-  uint32_t startT;
-
+  uint32_t startT, endT, pulseT, micros;
+  micros = 60;
+  float distance;
   uint32_t times[2];
 
   struct timespec start;
@@ -18,6 +19,7 @@ int main(){
     perror("[ERROR]Couldn't initialize GPIO interface.");
     exit(1);
   }
+ 
   // From the GPIO 27 pin we are going to read (ECHO 3V)
   gpioSetMode(ECHO, PI_INPUT);
   // From the GPIO 25 pin we are going to send a TTL pulse (TRIG)
@@ -25,42 +27,52 @@ int main(){
   
   printf("GPIO %d level is: %d\n", ECHO, gpioRead(ECHO));
   // Sends a 10us pulse to the TRIG
+while (1) {
   gpioTrigger(TRIG, 10, 1);
+
+  while (gpioRead(ECHO) == 0) {}
   startT = gpioTick();
+  while (gpioRead(ECHO) == 1) {}
+  endT = gpioTick();
+  
+  pulseT = endT - startT;
 
-  gpioSetAlertFuncEx(ECHO, flightTime, &start);
-  gpioSleep(PI_TIME_RELATIVE, 3, 0);
+  distance = (pulseT / 2) * 0.034;
+  /*gpioSetAlertFuncEx(ECHO, flightTime, &start);*/
+  /*gpioSleep(PI_TIME_RELATIVE, 3, 0);*/
 
-  printf("Tempo di volo: %d\n", times[0]-times[1]);
+  printf("Tempo di volo: %duS\nDistanza: %f\n", pulseT, distance);
+  gpioSleep(PI_TIME_RELATIVE, 0, 60000);
+}
   gpioTerminate();
 }
 
 
-void flightTime(int gpio, int level, uint32_t tick, void *userData) {
-  uint32_t time = 0;
+/*void flightTime(int gpio, int level, uint32_t tick, void *userData) {*/
+/*  uint32_t time = 0;*/
   /* uint32_t startT = 0; */
-  //uint32_t * times = *((uint32_t**)userData);
+/*  //uint32_t * times = *((uint32_t**)userData);*/
   /* startT = *((uint32_t*)userData); */
   /* printf("%d\n %d\n", startT, tick); */
-  struct timespec start = *((struct timespec * )userData);
-  
-
-  if (level == 1){
-    //times[0] = tick;
-    clock_gettime(CLOCK_MONOTONIC, &start);   
-  /*  if (startT < tick) {
-      time = tick - startT;
-      printf("Tempo di volo: %d\n", time);
-    }
-    else {
-      time = (4294967295 - startT) + tick;
-      printf("Tempo di volo else: %d\n", time);
-    } */
-  } else {
-    //times[1] = tick;
-    struct timespec end;
-    clock_gettime(CLOCK_MONOTONIC, &end);
-    printf("%ld.%ld\n", end.tv_sec - start.tv_sec, end.tv_nsec-start.tv_nsec);
-    
-  }
-}
+/*  struct timespec start = *((struct timespec * )userData);*/
+/**/
+/**/
+/*  if (level == 1){*/
+/*    //times[0] = tick;*/
+/*    clock_gettime(CLOCK_MONOTONIC, &start);   */
+/*  /*  if (startT < tick) {*/
+/*      time = tick - startT;*/
+/*      printf("Tempo di volo: %d\n", time);*/
+/*    }*/
+/*    else {*/
+/*      time = (4294967295 - startT) + tick;*/
+/*      printf("Tempo di volo else: %d\n", time);*/
+/*    } */
+/*  } else {*/
+/*    //times[1] = tick;*/
+/*    struct timespec end;*/
+/*    clock_gettime(CLOCK_MONOTONIC, &end);*/
+/*    printf("%ld.%ld\n", end.tv_sec - start.tv_sec, end.tv_nsec-start.tv_nsec);*/
+/**/
+/*  }*/
+/*}*/
